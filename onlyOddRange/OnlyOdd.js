bf7e67efc125ff2ba0ea2ed038a8e6a95975c530
clintsearl <clintsearl@gmail.com>
2019-05-31 16:19:56 -0600

    finished pulling odd numbers out of a given range

--- onlyOddRange/OnlyOdd.js
+++ onlyOddRange/OnlyOdd.js
@@ -1,14 +1,14 @@
 //Given a low and a high number, filter out and only return the odd numbers in an array form
-
+let l = 1
+let r = 5
 const range =(l, r)=>{
-    
-    
-    
-    
-    
-    
-    
-    return
+    let n = l
+    let arr = []
+    do{
+        arr.push(n)
+        n++;
+    }while(n> l && n<=r)
+    return arr.filter(odd => odd % 2 !== 0)
 }
 
-console.log(range(1, 5))
+console.log(range(1, 10))
